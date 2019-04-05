30fc93648006d0cad4a78ca6d14278bd5fce0427
Piotr <plachert@wp.pl>
2019-04-05 19:10:45 +0200

    refactored

---
+++ file_parsers.py
@@ -0,0 +1,68 @@
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
+from parser_utils import parse
+
+def parse_badges(sparkContext, path):
+    SCHEMA = StructType([ \
+        StructField("UserId", FloatType(), True), \
+        StructField("Name", StringType(), True), \
+        StructField("Date", StringType(), True)])
+    MAIN_TAG = 'badges'
+    return parse(sparkContext, path, SCHEMA, MAIN_TAG)
+
+def parse_comments(sparkContext, path):
+    SCHEMA = StructType([ \
+        StructField("Id", FloatType(), True), \
+        StructField("PostId", FloatType(), True), \
+        StructField("Score", FloatType(), True), \
+        StructField("Text", StringType(), True), \
+        StructField("CreationDate", StringType(), True), \
+        StructField("UserID", FloatType(), True)])
+    MAIN_TAG = 'comments'
+    return parse(sparkContext, path, SCHEMA, MAIN_TAG)
+
+def parse_posts(sparkContext, path):
+
+    SCHEMA = StructType([ \
+        StructField("Id", FloatType(), True), \
+        StructField("PostTypeId", FloatType(), True), \
+        StructField("ParentID", FloatType(), True), \
+        StructField("AcceptedAnswerId", FloatType(), True), \
+        StructField("CreationDate", StringType(), True), \
+        StructField("Score", FloatType(), True), \
+        StructField("ViewCount", FloatType(), True), \
+        StructField("Body", StringType(), True), \
+        StructField("OwnerUserId", FloatType(), True), \
+        StructField("LastEditorUserId", FloatType(), True),
+        StructField("LastEditorDisplayName", StringType(), True), \
+        StructField("LastEditDate", StringType(), True), \
+        StructField("LastActivityDate", StringType(), True),
+        StructField("CommunityOwnedDate", StringType(), True), \
+        StructField("ClosedDate", StringType(), True), \
+        StructField("Title", StringType(), True),
+        StructField("Tags", StringType(), True), \
+        StructField("AnswerCount", FloatType(), True), \
+        StructField("CommentCount", FloatType(), True), \
+        StructField("FavoriteCount", FloatType(), True)])
+    MAIN_TAG = 'posts'
+    return parse(sparkContext, path, SCHEMA, MAIN_TAG)
+
+
+
+def parse_users(sparkContext, path):
+    SCHEMA = StructType([ \
+        StructField("Id", FloatType(), True), \
+        StructField("Reputation", FloatType(), True), \
+        StructField("CreationDate", StringType(), True), \
+        StructField("DisplayName", StringType(), True), \
+        StructField("LastAccessDate", StringType(), True), \
+        StructField("WebsiteUrl", StringType(), True), \
+        StructField("Location", StringType(), True), \
+        StructField("AboutMe", StringType(), True), \
+        StructField("Views", FloatType(), True), \
+        StructField("UpVotes", FloatType(), True),
+        StructField("DownVotes", FloatType(), True), \
+        StructField("EmailHash", StringType(), True), \
+        StructField("Age", FloatType(), True)])
+    MAIN_TAG = 'users'
+    return parse(sparkContext, path, SCHEMA, MAIN_TAG)
+
---
+++ parser_utils.py
@@ -0,0 +1,36 @@
+from pyspark.sql import Row
+from xml.etree.ElementTree import fromstring
+
+def filter_headers(RDD, main_tag):
+    def filter_crap(s):
+        start = "<" + main_tag + ">"
+        end = "</" + main_tag + ">"
+        if s.startswith("<?xml version=") or s == start or s == end:
+            return False
+        else:
+            return True
+    filtered_RDD = RDD.filter(filter_crap)
+    return filtered_RDD
+
+def xml_to_dict(s, required_keys=[]):
+    tree = fromstring(s)
+    temp = dict((x, y) for x, y in tree.items())
+    temp_keys = temp.keys()
+    for key in required_keys:
+        if key['name'] in temp_keys:
+            if key['type'] == 'string':
+                temp[key['name']] = str(temp[key['name']])
+            elif key['type'] == 'float':
+                temp[key['name']] = float(temp[key['name']])
+            elif key['type'] == 'integer':
+                temp[key['name']] = float(temp[key['name']])
+        else:
+            temp[key['name']] = None
+    return temp
+
+def parse(sparkContext, path, SCHEMA, MAIN_TAG):
+    text = sparkContext.textFile(path)
+    no_headers = filter_headers(text, MAIN_TAG)
+    parsed_to_dict = no_headers.map(lambda x: xml_to_dict(x, SCHEMA.jsonValue()['fields']))
+    parsed = parsed_to_dict.map(lambda x: Row(**x))
+    return parsed.toDF(schema=SCHEMA)
